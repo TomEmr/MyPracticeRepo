3539915f9fc18caa139c088b8e4ae25230dd9b12
Tomas Emr <tommy.emr@gmail.com>
2023-06-27 11:00:19 +0200

    Spring advanced initialize commit

---
+++ SprindAdvanced/src/main/java/com/example/sprindadvanced/SprindAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprindadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprindAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprindAdvancedApplication.class, args);
+    }
+
+}
