c904b159eb2b8d8dd370110f1f8eec1c488d6b9a
Tomas Emr <tommy.emr@gmail.com>
2023-07-15 16:26:02 +0200

    Algo practise with comments done

---
+++ Algos/src/ShortestWord.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+//Simple, given a string of words, return the length of the shortest word(s).
+public class ShortestWord {
+
+        public static void main(String[] args) {
+            System.out.println(dummySolution("I am Robin Hood"));
+            System.out.println(dummySolution("bitcoin take over the world maybe who knows perhaps"));
+            System.out.println(dummySolution("turns out random test cases are easier than writing out basic ones"));
+            System.out.println("-----");
+            System.out.println(coolSolution("I am Robin Hood"));
+            System.out.println(coolSolution("bitcoin take over the world maybe who knows perhaps"));
+            System.out.println(coolSolution("turns out random test cases are easier than writing out basic ones"));
+
+        }
+
+        public static int dummySolution(String s){
+            String[] arr = s.split(" ");
+            int min = Integer.MAX_VALUE;
+            for (String str : arr) {
+                if (str.length() < min){
+                    min = str.length();
+                }
+            }
+            return min;
+        }
+
+        public static int coolSolution(String s){
+            return Arrays.stream(s.split(" "))
+                    .mapToInt(String::length)
+                    .min()
+                    .getAsInt();
+        }
+}
