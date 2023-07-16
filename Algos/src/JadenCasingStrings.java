65c9d948c07d1182dbf5cc845a079d9a0f270f9d
Tomas Emr <tommy.emr@gmail.com>
2023-07-16 12:34:45 +0200

    Algo practise with comments done

---
+++ Algos/src/JadenCasingStrings.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class JadenCasingStrings {
+
+    public static void main(String[] args) {
+
+        System.out.println(toJadenCase("How can mirrors be real if our eyes aren't real"));
+        System.out.println(coolSolution("How can mirrors be real if our eyes aren't real"));
+    }
+
+    public static String toJadenCase(String phrase) {
+        // TODO put your code below this comment
+        if (phrase == null || phrase.isEmpty()){
+            return null;
+        }
+        String[] arr = phrase.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s.substring(0, 1).toUpperCase()).append(s.substring(1)).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static String coolSolution(String phrase){
+        return Arrays.stream(phrase.split(" "))
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .reduce((a, b) -> a + " " + b)
+                .orElse(null);
+    }
+
+}
