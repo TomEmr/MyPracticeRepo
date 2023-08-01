6f9b7d1223fc1ae364297d4328d3ecce7a55d3eb
Tomas Emr <tommy.emr@gmail.com>
2023-08-01 13:26:41 +0200

    Algo practise with comments done

---
+++ Algos/src/CompareTheTriplets.java
@@ -0,0 +1,118 @@
+import java.util.Arrays;
+
+public class CompareTheTriplets {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(compareTheTriplets(new int[]{1, 2, 3}, new int[]{3, 2, 1})));
+        System.out.println(Arrays.toString(compareTheTriplets(new int[]{17, 28, 30}, new int[]{99, 16, 8})));
+        System.out.println("----------------");
+        System.out.println(Arrays.toString(switchSolution(new int[]{1, 2, 3}, new int[]{3, 2, 1})));
+        System.out.println(Arrays.toString(switchSolution(new int[]{17, 28, 30}, new int[]{99, 16, 8})));
+
+    }
+
+    public static int[] compareTheTriplets(int[] a, int[] b) {
+        int[] result = new int[2];
+        int alicePoint = 0;
+        int bobPoint = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > b[i]) {
+                alicePoint++;
+            } else if (a[i] < b[i]) {
+                bobPoint++;
+            }
+        }
+        result[0] = alicePoint;
+        result[1] = bobPoint;
+        return result;
+    }
+
+    public static int[] switchSolution(int[] a, int[] b) {
+        int[] result = new int[2];
+        for (int i = 0; i < a.length; i++) {
+            switch (Integer.compare(a[i], b[i])) {
+                case 1:
+                    result[0]++;
+                    break;
+                case -1:
+                    result[1]++;
+                    break;
+            }
+        }
+        return result;
+    }
+}
+
+//    Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+//
+//        The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+//
+//        The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+//
+//        If a[i] > b[i], then Alice is awarded 1 point.
+//        If a[i] < b[i], then Bob is awarded 1 point.
+//        If a[i] = b[i], then neither person receives a point.
+//        Comparison points is the total points a person earned.
+//
+//        Given a and b, determine their respective comparison points.
+//
+//        Example
+//
+//        a = [1, 2, 3]
+//        b = [3, 2, 1]
+//        For elements *0*, Bob is awarded a point because a[0] .
+//        For the equal elements a[1] and b[1], no points are earned.
+//        Finally, for elements 2, a[2] > b[2] so Alice receives a point.
+//        The return array is [1, 1] with Alice's score first and Bob's second.
+//
+//        Function Description
+//
+//        Complete the function compareTriplets in the editor below.
+//
+//        compareTriplets has the following parameter(s):
+//
+//        int a[3]: Alice's challenge rating
+//        int b[3]: Bob's challenge rating
+//        Return
+//
+//        int[2]: Alice's score is in the first position, and Bob's score is in the second.
+//        Input Format
+//
+//        The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+//        The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+//
+//        Constraints
+//
+//        1 ≤ a[i] ≤ 100
+//        1 ≤ b[i] ≤ 100
+//        Sample Input 0
+//
+//        5 6 7
+//        3 6 10
+//        Sample Output 0
+//
+//        1 1
+//        Explanation 0
+//
+//        In this example:
+//
+//        Now, let's compare each individual score:
+//
+//        , so Alice receives  point.
+//        , so nobody receives a point.
+//        , so Bob receives  point.
+//        Alice's comparison score is , and Bob's comparison score is . Thus, we return the array .
+//
+//        Sample Input 1
+//
+//        17 28 30
+//        99 16 8
+//        Sample Output 1
+//
+//        2 1
+//        Explanation 1
+//
+//        Comparing the  elements,  so Bob receives a point.
+//        Comparing the  and  elements,  and  so Alice receives two points.
+//        The return array is .
