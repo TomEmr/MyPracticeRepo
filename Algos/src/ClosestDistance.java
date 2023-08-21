48da46012e0cf5135c569a253ad5a6474fcebe2e
Tomas Emr <tommy.emr@gmail.com>
2023-08-21 11:37:08 +0200

    Algo practise with comments done

---
+++ Algos/src/ClosestDistance.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class ClosestDistance {
+
+    public static void main(String[] args) {
+        System.out.println(closestDistance(new int[]{1, 5, 61, 63}));
+        System.out.println(closestDistance(new int[]{1, 5, 61, 93, 100}));
+    }
+    public static int closestDistance(int[] arr) {
+        if (arr.length < 2) {
+            return -1;
+        }
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            int diff = Math.abs(arr[i] - arr[i - 1]);
+            if (diff < min) {
+                min = diff;
+            }
+        }
+        return min;
+    }
+}
