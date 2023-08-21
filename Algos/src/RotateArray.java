2b0f4a1cdd43ca4a515cd23a0e4cd31ea6230c96
Tomas Emr <tommy.emr@gmail.com>
2023-08-21 12:04:53 +0200

    Algo practise with comments done

--- Algos/src/RotateArray.java
+++ Algos/src/RotateArray.java
@@ -1,14 +1,33 @@
+import java.util.Arrays;
+
 public class RotateArray {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3)));
+    }
 
     public static int[] rotate(int[] nums, int k) {
         int[] result = new int[nums.length];
         int index = 0;
-        for (int i = nums.length - k; i < nums.length; i++) {
+        for (int i = k; i < nums.length; i++) {
             result[index++] = nums[i];
         }
-        for (int i = 0; i < nums.length - k; i++) {
+        for (int i = 0; i < k; i++) {
             result[index++] = nums[i];
         }
         return result;
     }
+
+//    public static List<Integer> rotLeft(List<Integer> a, int d) {
+//        // Write your code here
+//        List<Integer> res = new ArrayList<>();
+//        for (int i = d; i < a.size(); i++) {
+//            res.add(a.get(i));
+//        }
+//        for (int i = 0; i < d; i++) {
+//            res.add(a.get(i));
+//        }
+//        return res;
+//    }
 }
