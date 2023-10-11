ed2c70b85fa19fc09f53f1c5c0d1dd05e89050ee
Tomas Emr <tommy.emr@gmail.com>
2023-10-11 12:49:40 +0200

    component buttons to delete movie, delete all movies and reload all movies

---
+++ netflix-app/src/components/ReloadAllMovies.js
@@ -0,0 +1,10 @@
+
+const ReloadAllMovies = (props) => {
+    return (
+        <div>
+            <button className="main-reload-button" onClick={props.reloadAllMovies}>Reload All Movies</button>
+        </div>
+    );
+}
+
+export default ReloadAllMovies;
