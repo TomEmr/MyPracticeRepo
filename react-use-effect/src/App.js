94325e770f2552ea12a8052b6bf36bf18bf73275
Tomas Emr <tommy.emr@gmail.com>
2023-10-12 12:35:15 +0200

    initialize commit

---
+++ react-use-effect/src/App.js
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+    return (
+        <div>
+        <h1>React 18</h1>
+        <p>React 18 is the next version of React.</p>
+        </div>
+    )
+}
+export default App;
