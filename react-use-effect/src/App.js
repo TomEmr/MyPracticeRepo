764b7379b989828f1ef7547f2abed5c5b49403ca
Tomas Emr <tommy.emr@gmail.com>
2023-11-06 21:42:26 +0100

    resize window using useState and useEffect

--- react-use-effect/src/App.js
+++ react-use-effect/src/App.js
@@ -1,10 +1,30 @@
-
+import {useEffect, useState} from "react";
 
 const App = () => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    });
+
+    useEffect(() => {
+        const handleResize = () => setHeight(window.innerHeight);
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    });
+
     return (
         <div>
-        <h1>React 18</h1>
-        <p>React 18 is the next version of React.</p>
+            <h1>Window Width: {windowWidth}</h1>
+            <h1>Window Height: {height}</h1>
         </div>
     )
 }
