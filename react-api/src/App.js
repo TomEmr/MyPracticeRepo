e8f7518a7a5a9ae53dfca80a44bf6136f40b3bcf
Tomas Emr <tommy.emr@gmail.com>
2023-11-24 11:12:55 +0100

    added space station and multiple return pages

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,23 +1,31 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Fetch from "./Fetch";
 import Home from "./Home";
+import SpaceStation from "./SpaceStation";
+import MultipleReturn from "./Multiple-return";
+import './index.css';
+
 const App = () => {
 
     return (
-        <Router>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/fetch">Fetch</Link>
+        <div className="app-container">
+            <Router>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/fetch">Fetch</Link>
+                    <Link to="/space-station">Space Station</Link>
+                    <Link to="/multiple-return">Multiple Return</Link>
 
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/fetch" element={<Fetch />} />
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/fetch" element={<Fetch/>}/>
+                    <Route path="/space-station" element={<SpaceStation/>}/>
+                    <Route path="/multiple-return" element={<MultipleReturn/>}/>
 
-            </Routes>
-        </Router>
+                </Routes>
+            </Router>
+        </div>
     )
 }
-
-
 export default App
