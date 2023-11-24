0c1a39444a5a5e9f7d9c897ea26ae7542933ecce
Tomas Emr <tommy.emr@gmail.com>
2023-11-24 15:02:13 +0100

    movie project main components

---
+++ react-api/src/components/Movie.js
@@ -0,0 +1,39 @@
+import "./movie.css";
+import {useState} from "react";
+import allMovies from "../movie_project/allMovies";
+import categories from "../movie_project/categories";
+
+const Movie = () => {
+
+    const [movieType, setMovieType] = useState("komedie");
+
+    const movies = allMovies.filter((movie) => movie.category === movieType);
+
+    console.log(movies);
+
+    return (
+        <div className="movie">
+            <div className="buttons">
+                {categories.map((category, index) => {
+                    return (
+                        <button className="one-button" key={index} onClick={() => setMovieType(category)}>{category}</button>
+                    )
+                })}
+            </div>
+            <div className="allMovies">
+                {movies.map((movie) => {
+                    return (
+                        <div className="movie-card" key={movie.id}>
+                            <img src={movie.image} alt={movie.title}/>
+                            <h3>{movie.title}</h3>
+                            <p>{movie.description}</p>
+                            <p>{movie.tags}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Movie;
