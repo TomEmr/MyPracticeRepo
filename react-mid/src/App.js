55c5edbffd9ae7c97f734d76044540707cf1a58b
Tomas Emr <tommy.emr@gmail.com>
2023-11-29 11:10:21 +0100

    main app component

--- react-mid/src/App.js
+++ react-mid/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
+import BasicForm from "./components/BasicForm";
 import './index.css';
 
 function App() {
@@ -9,11 +10,13 @@ function App() {
             <nav className="navbar">
                 <ul>
                     <li><Link to="/">Home</Link></li>
+                    <li><Link to="/basic-form">Basic Form</Link></li>
                     {/* Add more navigation links here */}
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/basic-form" element={<BasicForm />} />
                 {/* Add more routes here */}
             </Routes>
         </Router>
